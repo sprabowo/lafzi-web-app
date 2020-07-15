102137ab78dde991727cfcb1adbf19ea35dc5ba0
Sigit Prabowo <sgt@prbw.net>
2020-07-15 13:25:04 +0700

    fix: minor typo

--- src/App.js
+++ src/App.js
@@ -209,7 +209,7 @@ export const Fetcher = React.memo(({ ayats, status, info, onLoadMore }) => {
 const Footer = () => {
   return (
     <Flex mt="24px" flex="0 0 100%" flexWrap="wrap" justifyContent="center" maxWidth="100%">
-      <Link color="gray.300" textShadow="0px 2px 2px rgba(0,0,0,.1)" href="https://github.com/sprabowo/lafzi-web">{`< source code />`}</Link>
+      <Link color="gray.300" textShadow="0px 2px 2px rgba(0,0,0,.1)" href="https://github.com/sprabowo/lafzi-web-app">{`< source code />`}</Link>
     </Flex>
   )
 };
